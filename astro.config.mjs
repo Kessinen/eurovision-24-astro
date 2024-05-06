db918e0b25f5f3419d28137aed2af75e6f73aca6
Esa Kataja <ekataja@gmail.com>
2024-05-06 19:39:44 +0300

    CHANCE Switch to sqlite

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-
 import node from "@astrojs/node";
+
+import db from "@astrojs/db";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [tailwind(), db()],
   output: "server",
   adapter: node({
     mode: "standalone"
---
+++ db/config.ts
@@ -0,0 +1,43 @@
+import { defineDb, defineTable, column } from 'astro:db';
+
+const Users = defineTable({
+  columns: {
+    id: column.number({ primaryKey: true, autoIncrement: true }),
+    username: column.text(),
+    password: column.text(),
+    profile_picture: column.number({ default: 1 }),
+    isAdmin: column.boolean({ default: false }),
+  }
+})
+
+const Participants = defineTable({
+  columns: {
+    id: column.number({ primaryKey: true }),
+    year: column.number(),
+    country: column.text(),
+    country_img: column.text(),
+    name: column.text(),
+    song: column.text(),
+    img: column.text(),
+    url: column.text(),
+    round_num: column.number(),
+    turn: column.number()
+  }
+})
+
+const Reviews = defineTable({
+  columns: {
+    id: column.number({ primaryKey: true }),
+    user_id: column.number({ references: () => Users.columns.id }),
+    participant_id: column.number({ references: () => Participants.columns.id }),
+    round_num: column.number(),
+    melody: column.number(),
+    performance: column.number(),
+    wardrobe: column.number()
+  }
+})
+
+// https://astro.build/db/config
+export default defineDb({
+  tables: { Users, Participants, Reviews }
+});
